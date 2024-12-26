b23baab80c29d81d65d9ad58c9ded03e0d84d72d
Juveria2 <163538512+Juveria2@users.noreply.github.com>
2024-12-26 17:45:20 +0100

    Red Phase User Story#2(2)

--- test/test_cleaning_robot.py
+++ test/test_cleaning_robot.py
@@ -21,4 +21,10 @@ class TestCleaningRobot(TestCase):
         system.manage_cleaning_system()
         mock.assert_has_calls([call(system.RECHARGE_LED_PIN, GPIO.LOW), call(system.CLEANING_SYSTEM_PIN, GPIO.HIGH)])
 
-
+    @patch.object(IBS, "get_charge_left")
+    @patch.object(GPIO, "output")
+    def test_manage_cleaning_system_is_equal_or_less_than_10(self, mock: Mock, mock_ibs: Mock):
+        system = CleaningRobot()
+        mock_ibs.return_value = 9
+        system.manage_cleaning_system()
+        mock.assert_has_calls([call(system.RECHARGE_LED_PIN, GPIO.HIGH), call(system.CLEANING_SYSTEM_PIN, GPIO.LOW)])
